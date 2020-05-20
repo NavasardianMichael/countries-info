6aa0870599e83cb803e424bb067952e9abd8bf63
Navasardian Michael <navasardianmichael@gmail.com>
2020-05-21 00:54:56 +0400

    Updates

---
+++ precache-manifest.71e87ba9681a0d559fbcc6255ce9c15c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bf6e9fa6ce1a2a933b03565d210136",
+    "url": "/countries-info/index.html"
+  },
+  {
+    "revision": "da9402077b190b18cffa",
+    "url": "/countries-info/static/css/main.f8ae5815.chunk.css"
+  },
+  {
+    "revision": "9696815457c7e06dfca6",
+    "url": "/countries-info/static/js/2.2ca0c068.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/countries-info/static/js/2.2ca0c068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da9402077b190b18cffa",
+    "url": "/countries-info/static/js/main.1e5117d0.chunk.js"
+  },
+  {
+    "revision": "1f0c607c8cea87fce709",
+    "url": "/countries-info/static/js/runtime-main.2c5b11da.js"
+  }
+]);
